ef53515028ffb93330a72913f75b2719bfca7d41
SUContent <134996680+SUContent@users.noreply.github.com>
2025-06-16 14:47:10 +0300

    Initial Commit
    Update index.html
    
    Add input validation to subtract()
    cherry-pick made

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ function add(a, b) {
 }
 
 function subtract(a, b) {
-  return a - b;
+  if (a==='') {
+    return "The left field is empty"
+  } else if (b==='') {
+    return "The right field is empty"
+  } else {
+    return a - b;
+  }
 }
 
 document.getElementById('btn-add').addEventListener('click', () => {
@@ -13,7 +19,14 @@ document.getElementById('btn-add').addEventListener('click', () => {
 });
 
 document.getElementById('btn-subtract').addEventListener('click', () => {
-  const a = Number(document.getElementById('a').value);
-  const b = Number(document.getElementById('b').value);
+
+  let a = document.getElementById('a').value;
+  if (a !== '') {
+    a = Number(a);
+  }
+  let b = document.getElementById('b').value;
+  if (b !== '') {
+    b = Number(b);
+  }
   document.getElementById('result').textContent = subtract(a, b);
 });
