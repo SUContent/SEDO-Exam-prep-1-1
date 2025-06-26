9b2e674da183316ee9f42b7a60e0dc63313058a2
Viktoria Stoycheva <vikivikisto@abv.bg>
2025-06-26 17:26:05 +0300

    This is the first pull request
    [Issue #2](https://github.com/ViktoriaSvt/SEDO-Exam-prep-1-1/issues/2)

--- script.js
+++ script.js
@@ -5,15 +5,24 @@ function add(a, b) {
 function subtract(a, b) {
   return a - b;
 }
-
+//ONE
+//FOUR
+//SEVEN
 document.getElementById('btn-add').addEventListener('click', () => {
   const a = Number(document.getElementById('a').value);
   const b = Number(document.getElementById('b').value);
   document.getElementById('result').textContent = add(a, b);
 });
 
+//TWO
+//FIVE
+//EIGHT
 document.getElementById('btn-subtract').addEventListener('click', () => {
   const a = Number(document.getElementById('a').value);
   const b = Number(document.getElementById('b').value);
   document.getElementById('result').textContent = subtract(a, b);
 });
+
+//THREE
+//SIX
+//NINE
