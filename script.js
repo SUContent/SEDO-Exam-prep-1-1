f5771e4ab320538c1d0226cd66ff3137a3d7c6a6
Viktoria Stoycheva <vikivikisto@abv.bg>
2025-06-26 17:09:24 +0300

    This is a combination of 3 commits.

--- script.js
+++ script.js
@@ -5,15 +5,19 @@ function add(a, b) {
 function subtract(a, b) {
   return a - b;
 }
-
+//ONE
 document.getElementById('btn-add').addEventListener('click', () => {
   const a = Number(document.getElementById('a').value);
   const b = Number(document.getElementById('b').value);
   document.getElementById('result').textContent = add(a, b);
 });
 
+//TWO
+
 document.getElementById('btn-subtract').addEventListener('click', () => {
   const a = Number(document.getElementById('a').value);
   const b = Number(document.getElementById('b').value);
   document.getElementById('result').textContent = subtract(a, b);
 });
+
+//THREE
