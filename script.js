fe29a31affec0e7d2ee2dfe45aec1fe7d1ee803d
Teodosi Kachakov <teo.kachakov@gmail.com>
2025-10-23 17:46:07 +0300

    Bug is fixed, some changes are made

--- script.js
+++ script.js
@@ -6,14 +6,19 @@ function subtract(a, b) {
   return a - b;
 }
 
-document.getElementById('btn-add').addEventListener('click', () => {
-  const a = Number(document.getElementById('a').value);
-  const b = Number(document.getElementById('b').value);
-  document.getElementById('result').textContent = add(a, b);
+// Bug: Fix substract when inputs empty
+
+document.getElementById("btn-add").addEventListener("click", () => {
+  const a = Number(document.getElementById("a").value);
+  const b = Number(document.getElementById("b").value);
+  document.getElementById("result").textContent = add(a, b);
 });
 
-document.getElementById('btn-subtract').addEventListener('click', () => {
-  const a = Number(document.getElementById('a').value);
-  const b = Number(document.getElementById('b').value);
-  document.getElementById('result').textContent = subtract(a, b);
+// Bug: Fix substract when inputs empty 2
+document.getElementById("btn-subtract").addEventListener("click", () => {
+  const a = Number(document.getElementById("a").value);
+  const b = Number(document.getElementById("b").value);
+  document.getElementById("result").textContent = subtract(a, b);
 });
+
+// Bug: Fix substract when inputs empty 3
