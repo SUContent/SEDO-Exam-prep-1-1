81a800cd850fef06f2a72f803a4a2236215067ac
Kamen Kadiyski <kamen.kadiyski@gmail.com>
2026-06-13 20:41:14 +0100

    fixed functionality

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ function add(a, b) {
 function subtract(a, b) {
   return a - b;
 }
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
 
 document.getElementById('btn-add').addEventListener('click', () => {
   const a = Number(document.getElementById('a').value);
